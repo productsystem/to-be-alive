3684bdff03dab1faa586ce7d44d987ca2f5cc18f
Prajit P <prajitpradeep@gmail.com>
2025-12-29 01:06:13 +0530

    alignment

--- script.js
+++ script.js
@@ -55,10 +55,17 @@ async function loadFile(section, filename) {
 /* ---------- WRITING ENGINE ---------- */
 function parse(text) {
     return text
+        // alignment blocks
+        .replace(/\[left\]([\s\S]*?)\[\/left\]/g, `<div class="align-left">$1</div>`)
+        .replace(/\[center\]([\s\S]*?)\[\/center\]/g, `<div class="align-center">$1</div>`)
+        .replace(/\[right\]([\s\S]*?)\[\/right\]/g, `<div class="align-right">$1</div>`)
+
+        // inline effects
         .replace(/\[color:(.*?)\](.*?)\[\/color\]/gs, `<span class="c-$1">$2</span>`)
         .replace(/\[rainbow\](.*?)\[\/rainbow\]/gs, `<span class="rainbow">$1</span>`)
         .replace(/\[bounce\](.*?)\[\/bounce\]/gs, `<span class="bounce">$1</span>`);
 }
+
 
 // default
 showSection("posts");
